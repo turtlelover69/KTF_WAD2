00c0281383fff7108d874a3e46435fe6eb46faae
Aaron <tan.aaron.2019@sis.smu.edu.sg>
2020-11-04 14:45:46 +0800

    Aaron/updated apiConnect.js

--- js/apiConnect.js
+++ js/apiConnect.js
@@ -3,11 +3,11 @@ getIngredients:search api for recipes given a list of ingredients. Takes in list
 youtubeLink:search an asmr video for the recipe selected
 getSummary:get the summary of a recipe given its id
 getDetail:get detail of a recipe gen its id
-
 */
-
 var youtubeAPI='';
-var spoonAPI='c168142caa444879a3b6d6892ed09067';
+var spoonAPI='';
+
+
 
 
 function call_api(input,functionName) {
@@ -19,31 +19,74 @@ function call_api(input,functionName) {
 
         if( this.readyState == 4 && this.status == 200 ) {
         actionFunction(input,functionName);            
+
         }
+
     }
 
     // Step 3
     url=urlFunction(input,functionName);
+    
+    
     request.open("GET", url, true);
+
     // Step 4
     request.send();
 }
 
 
-function urlFunction(input,functionName) { 
+
+
+function urlFunction(input,functionName) {
+ 
     if (functionName=="getIngredients"){
-        var base="https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
-        var end='&number=10&apiKey=';
-        var ingredient;
-        for (ingredient of input){
-            if (ingredient==input[0]){
-                base=base+ingredient;
+        var base="https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=10&apiKey=";
+
+
+        var ingredients='';
+        var diet='';
+        var intolerances='';
+        var cuisines=''
+
+        if (input[0].length >0){
+            var ingredient;
+            for (ingredient in input[0]){
+                if (ingredient==0){
+                    ingredients='&includeIngredients='+input[0][ingredient];
+                }
+                else {
+                    ingredients=ingredients +","+ input[0][ingredient];
+                }
+            }}
+
+        if (input[1].length >0){
+            diet="&diet"+input[1];
             }
-            else {
-                base=base +",+"+ ingredient;
-            }
-        }
-        return (base+end+spoonAPI)}
+
+        if (input[2].length >0){
+            var intolerance;
+            for (intolerance in input[2]){
+                if (intolerance==0){
+                    intolerances='&intolerances='+input[2][intolerance];
+                }
+                else {
+                    intolerances=intolerances +","+ input[2][intolerance];
+                }
+            }}
+    
+        if (input[3].length >0){
+            var cuisine;
+            for (cuisine in input[3]){
+                if (cuisine==0){
+                    cuisines='&cuisine='+input[3][cuisine];
+                }
+                else {
+                    cuisines=cuisines +","+ input[3][cuisine];
+                }
+            }}
+
+     
+        return (base+spoonAPI+ingredients+diet+intolerances+cuisines)}
 
     else if (functionName=="youtubeLink"){
         var base="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=asmr+chicken+rice";
@@ -53,6 +96,8 @@ function urlFunction(input,functionName) {
         for (word of recipe){
             base=base + "+" + word;
         }
+        
+
         return base+end+youtubeAPI
     }
 
@@ -68,12 +113,17 @@ function urlFunction(input,functionName) {
         var end="/information?includeNutrition=false&apiKey=";
         return base+input+end+spoonAPI
     }
-}
+
+
+
+
+    }
 
 
 function actionFunction(input,functionName){
     if (functionName=="getIngredients"){
         var response_json = JSON.parse(this.responseText);
+
     }
 
     else if (functionName=="youtubeLink"){
@@ -87,4 +137,7 @@ function actionFunction(input,functionName){
     else if (functionName=="getDetail"){
         var response_json = JSON.parse(this.responseText);
     }
+
+
+
 }
