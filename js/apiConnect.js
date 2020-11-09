9872f83c77797ec9c5ceb0e1224cc7bd6bc6729a
Aaron T <atye1997@gmail.com>
2020-11-10 00:57:46 +0800

    Aaron/added stack function to revert changes

--- js/apiConnect.js
+++ js/apiConnect.js
@@ -1,6 +1,5 @@
-
 const youtubeAPI='';
-const spoonAPI='';
+const spoonAPI='c168142caa444879a3b6d6892ed09067';
 
 
 /*function names for call_api
@@ -69,7 +68,9 @@ function urlFunction(input,functionName) {
                 else {
                     cuisines += ","+ input.cuisine[index];
                 }
-            }}
+            }
+        }
+
         var final_url = base+spoonAPI+ingredients+diet+intolerances+cuisines;
         console.log(final_url)
         return (base+spoonAPI+ingredients+diet+intolerances+cuisines)
@@ -84,70 +85,17 @@ function urlFunction(input,functionName) {
             base=base + "+" + word;
         }
         
-        return base+end+youtubeAPI
+        return base+end+youtubeAPI;
     }
 
     else if (functionName=="getDetail"){
         var base="https://api.spoonacular.com/recipes/";
         var end="/information?includeNutrition=true&apiKey=";
-        return base+input+end+spoonAPI
+        return base+input+end+spoonAPI;
     }
 }
 
 
-function actionFunction(xml,functionName){
-    if (functionName=="getIngredients"){
-        var parseJSON = JSON.parse(xml.responseText);
-        document.getElementById('card-columns').innerHTML='';
-        var base='';
-        var info = parseJSON.results;
-        // console.log(info);
-        var recipe;
-        console.log(info)
-        for (recipe of info){
-            // console.log(recipe);
-            var card= `
-                <div class="card col" style=" background-color: white">
-                    <img class="card-img-top " src="${recipe.image}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title d-flex justify-content-center border border-dark">${recipe.title}</h5>
-                
-                            <div class= "d-flex justify-content-center">
-                                <div class="card-text" style="display: inline;margin-right: 10px;">${recipe.readyInMinutes} min</div>
-                                <i class="fas fa-stopwatch" style="display: inline;"></i>
-                            </div>
-                    
-                            <div class= "d-flex justify-content-center">
-                                <div class="card-text" style="display: inline; margin-right: 10px;">${recipe.spoonacularScore} / 100</div>
-                                <i class="fas fa-star" style="display: inline;"></i>
-                            </div>
-                    
-                            <div class= "d-flex justify-content-center">
-                                <div class="card-text" style="display: inline;margin-right: 10px;">${recipe.missedIngredientCount} missing ingredients</div>
-                                <i class="far fa-question-circle" style="display: inline;"></i>
-                            </div> 
-                        </div>
-                </div>
-            `;
-            base+=card;
-            document.getElementById('card-columns').innerHTML=base;
-        }
-
-        
 
 
-    }
 
-    else if (functionName=="youtubeLink"){
-        var response_json = JSON.parse(this.responseText);
-    }
-
-    else if (functionName=="getSummary"){
-        var response_json = JSON.parse(this.responseText);
-    }
-
-    else if (functionName=="getDetail"){
-        var response_json = JSON.parse(this.responseText);
-    }
-
-}
--- js/categories.js
+++ js/categories.js
@@ -9,6 +9,12 @@ const categories = {
     cuisinesList: {list:["african","american","british","cajun","caribbean","chinese","eastern european","european","french","german","greek","indian","irish","italian","japanese","jewish","korean","latin american","mediterranean","mexican","middle eastern","nordic","southern","spanish","thai","vietnamese"],name: "Cuisines", imageurl: "images/cuisines.jpg",color:"grey"} 
 }
 
+//Initialize temporary storage for selected tags
+sessionStorage.setItem("storage", JSON.stringify(
+    {ingredient:[],
+    diet:[],
+    intolerance:[],
+    cuisine:[]}));
 
 //This function dynamically populate all the categories(meats,vegetables,etc) and also include checkbox
 function populate_categories()
@@ -87,6 +93,7 @@ function populate_checkbox(selected_item){
     }
 }
 
+
 // Retrieve value from search box
 function populate_searchbox(){
     // Remember to validate the input!!!
@@ -122,6 +129,92 @@ function remove_all_tags(){
 //     console.log(result)
 // }
 
+
+// Retrieve the API call and populate the cards in index.html
+//getIngredients,youtubeLink,getSummary,getDetail
+function actionFunction(xml,functionName){
+    if (functionName=="getIngredients"){
+        var parseJSON = JSON.parse(xml.responseText);
+        document.getElementById('card-columns').innerHTML='';
+        var base='';
+        var recipe;
+        var info = parseJSON.results;
+        var temporary_storage = JSON.parse(sessionStorage.getItem("storage"));
+        if(temporary_storage.ingredient.length>1 && info.length==0){
+            no_result_page()
+        }
+
+        for(recipe of info){
+            var card= `
+                <div class="card col" style=" background-color: white">
+                    <img class="card-img-top " src="${recipe.image}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title d-flex justify-content-center border border-dark">${recipe.title}</h5>
+                
+                            <div class= "d-flex justify-content-center">
+                                <div class="card-text" style="display: inline;margin-right: 10px;">${recipe.readyInMinutes} min</div>
+                                <i class="fas fa-stopwatch" style="display: inline;"></i>
+                            </div>
+                    
+                            <div class= "d-flex justify-content-center">
+                                <div class="card-text" style="display: inline; margin-right: 10px;">${recipe.spoonacularScore} / 100</div>
+                                <i class="fas fa-star" style="display: inline;"></i>
+                            </div>
+                    
+                            <div class= "d-flex justify-content-center">
+                                <div class="card-text" style="display: inline;margin-right: 10px;">${recipe.missedIngredientCount} missing ingredients</div>
+                                <i class="far fa-question-circle" style="display: inline;"></i>
+                            </div> 
+                        </div>
+                </div>
+            `;
+            base+=card;
+            document.getElementById('card-columns').innerHTML=base;
+        }
+        
+    }
+
+    else if (functionName=="youtubeLink"){
+        var response_json = JSON.parse(this.responseText);
+    }
+
+    else if (functionName=="getSummary"){
+        var response_json = JSON.parse(this.responseText);
+    }
+
+    else if (functionName=="getDetail"){
+        var response_json = JSON.parse(this.responseText);
+    }
+}
+
+//Page to display when there is no result
+function no_result_page(){
+    //Create a button that says "YES TAKE ME BACK!"
+    var return_template = `
+    <div id='error_page' style='text-align:center'>
+        <h1>Oops, there is no search result! Do you want to revert your changes?</h1>
+        <br>
+        <button type="button" class="btn btn-warning btn-outline-dark font-weight-bold btn-lg" onclick="no_result_action()">Yes! Take me back!!!</button>
+    </div>
+    `;
+
+    document.getElementById('error-msg').innerHTML = return_template;
+
+}
+
+function no_result_action(){
+    //Once clicked, call the temporary stack and pop the latest element
+    var temporary_storage = JSON.parse(sessionStorage.getItem("storage"));
+    var removed_tag = temporary_storage.ingredient.pop();
+    document.getElementById('error-msg').innerHTML = '';
+    remove_tag(`${removed_tag}_ingredient`);
+    //Then call the api again with the existing stack
+    sessionStorage.setItem("storage",JSON.stringify(temporary_storage));
+    call_api(temporary_storage,"getIngredients");
+    //Also remember to delete the apprioriate tag and uncheck the correct checkbox
+
+}
+
 //[START] Using mutation observer to gather all the ingredients and send to spoontaculous API
 const targetNode = document.getElementById('search_tags');
     // Options for the observer (which mutations to observe)
@@ -136,11 +229,14 @@ const current_tag_nodes = function(mutationsList, observer) {
         cuisine:[]
     }
     for(tag_node of tag_nodes){
-
         var splitted_tag = tag_node.id.split('_');
         var tag_name = splitted_tag[0];
         var tag_type = splitted_tag[1];
+        //Push into temporary storage to simulate stack operation later on
+        var temporary_storage = JSON.parse(sessionStorage.getItem("storage"));
         all_current_tags[tag_type].push(tag_name);
+        temporary_storage[tag_type].push(tag_name);
+        sessionStorage.setItem("storage", JSON.stringify(temporary_storage));
     }
     console.log(all_current_tags)
 
